4c3ca1c4fec5c5629c3f821418bc1442bc45cb87
wisyr <skynurak@gmail.com>
2016-10-26 13:39:32 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(window).on('load', function(){
+    setTimeout(function() {
+        $('.loadingAnimation').css({'display':'none'});
+        $('.content').fadeIn();
+        $('.mainContainer').css({'-webkit-filter': 'brightness(100%)', '-ms-filter': 'brightness(100%)', 'filter': 'brightness(100%)'});
+    }, 850);
+});
+
+(function(){
+$('.manageTodosButton').on('click', function(){
+    $('.manageTodosButton').toggleClass('revealCleanTodosButton');
+});
+})();
